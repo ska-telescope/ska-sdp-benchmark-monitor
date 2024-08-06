f7b525eee590559cd6a12202183fc2c739e6f881
Manuel Stutz <manuel.stutz@fhnw.ch>
2024-08-06 11:57:21 +0200

    SCOOP-312: Update minor stuff

--- benchmon/run/run_monitor.py
+++ benchmon/run/run_monitor.py
@@ -1,11 +1,23 @@
+import signal
 
 class RunMonitor:
     def __init__(self, args):
+        self.should_run = True
         self.save_dir = args.save_dir
         self.prefix = args.prefix
         self.sampling_freq = args.sampling_freq
         self.checkpoint = args.checkpoint
         self.verbose = args.verbose
 
+        # Setup SIGTERM handler for graceful termination
+        signal.signal(signal.SIGTERM, self.terminate)
+
     def run(self):
+        # run dool while OK
+        pass
+
+    def terminate(self):
+        # kill dool process gracefully
+        # postprocess data
+        # terminate
         pass
