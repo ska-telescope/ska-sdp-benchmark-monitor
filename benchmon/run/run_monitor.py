72200a1ca1c0e0c584058d5964239fd20eb6df96
aserhani <anass.serhani@inria.fr>
2024-11-25 11:49:34 +0100

    Add callgraph collection

--- benchmon/run/run_monitor.py
+++ benchmon/run/run_monitor.py
@@ -15,17 +15,28 @@ class RunMonitor:
         self.save_dir = args.save_dir
         self.prefix = args.prefix
 
-        self.filename = "sys_report.csv"
         self.sampling_freq = args.sampling_freq
         self.verbose = args.verbose
 
+        # System monitoring parameters
+        self.filename = "sys_report.csv"
         self.is_system = args.system
         self.system_sampling_interval = args.system_sampling_interval
 
+        # Power monitoring parameters
         self.pow_filename = "pow_report.csv"
         self.is_power = args.power
         self.power_sampling_interval = args.power_sampling_interval
 
+        # Profiling and callstack parameters
+        self.call_filename = "call_report.txt"
+        self.is_call = args.call
+        self.call_mode = args.call_mode
+        self.call_profiling_frequency = args.call_profiling_frequency
+        self.temp_perf_file = "_temp_perf.data"
+
+        # Enable sudo-g5k (for Grid5000 clusters)
+        self.sudo_g5k = "sudo-g5k" if args.sudo_g5k else ""
 
         # Mark the node with SLURM_NODEID == "0" as main node responsible for collecting all the different reports in the end
         self.is_benchmon_control_node = os.environ.get("SLURM_NODEID") == "0" if "SLURM_NODEID" in os.environ else False
@@ -73,6 +84,9 @@ class RunMonitor:
         if self.is_power:
             self.run_perf_pow()
 
+        if self.is_call:
+            self.run_perf_call()
+
         if self.is_system:
             self.dool_process.wait()
             print("Dool exited unexpectedly!")
@@ -114,15 +128,49 @@ class RunMonitor:
             fn.write(f"# {time.time()}\n")
 
         # Perf power command
-        perf_pow_cmd = ["sudo-g5k", "perf", "stat", "-A", "-a"] + event_flags + ["-I", f"{sampl_intv}"] + ["-x", ",", "--append", "-o", f"{filename}"]
+        perf_pow_cmd = [self.sudo_g5k, "perf", "stat", "-A", "-a"] + event_flags + ["-I", f"{sampl_intv}"] + ["-x", ",", "--append", "-o", f"{filename}"]
 
         if self.verbose:
             print(f"Starting perf-pow with command \"{' '.join(perf_pow_cmd)}\"")
 
-        # Run perf
+        # Run perf (power)
         self.perfpow_process = subprocess.Popen(perf_pow_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
+    def run_perf_call(self):
+        """
+        Profile and get the call graph
+        """
+        perf_call_cmd = [self.sudo_g5k, "perf", "record", "--running-time", "-T", "-a", "-F", f"{self.call_profiling_frequency}", "--call-graph", f"{self.call_mode}", "-o", f"{self.save_dir}/{self.temp_perf_file}"]
+
+        if self.verbose:
+            print(f"Starting perf-call with command \"{' '.join(perf_call_cmd)}\"")
+
+        # Get the conversion from monotonic to real time
+        monotonic = time.clock_gettime(time.CLOCK_MONOTONIC)
+        real = time.clock_gettime(time.CLOCK_REALTIME)
+        with open(f"{self.save_dir}/mono_to_real_file.txt", "w") as file:
+            file.write(f"{real - monotonic}\n")
+
+        # Run perf (call)
+        self.perfcall_process = subprocess.Popen(perf_call_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
     def terminate(self, signum=None, frame=None):
+        # kill perf (call)
+        if self.perfcall_process:
+            perfcall_children = [f"{child.pid}" for child in psutil.Process(self.perfcall_process.pid).children()]
+            kill_perf_pow_cmd = [self.sudo_g5k, "kill", "-15", f"{self.perfcall_process.pid}"] + perfcall_children
+            subprocess.run(kill_perf_pow_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+            if self.verbose:
+                print(f"Terminated perf (call) process on node \"{HOSTNAME}\".\nOutput: {self.perfcall_process.stdout.read()}")
+
+        # kill perf (power)
+        if self.perfpow_process:
+            perfpow_children = [f"{child.pid}" for child in psutil.Process(self.perfpow_process.pid).children()]
+            kill_perf_pow_cmd = [self.sudo_g5k, "kill", "-15", f"{self.perfpow_process.pid}"] + perfpow_children
+            subprocess.run(kill_perf_pow_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+            if self.verbose:
+                print(f"Terminated perf (pow) process on node \"{HOSTNAME}\".\nOutput: {self.perfpow_process.stdout.read()}")
+
         # kill dool process gracefully
         if self.dool_process and self.dool_process.poll() is None:
             self.dool_process.terminate()
@@ -130,13 +178,11 @@ class RunMonitor:
                 print(f"Terminated dool process on node \"{HOSTNAME}\".\nOutput: {self.dool_process.stdout.read()}")
             self.dool_process = None
 
-        # kill perf (power)
-        if self.perfpow_process:
-            perfpow_children = [f"{child.pid}" for child in psutil.Process(self.perfpow_process.pid).children()]
-            kill_perf_pow_cmd = ["sudo-g5k", "kill", "-15", f"{self.perfpow_process.pid}"] + perfpow_children
-            subprocess.run(kill_perf_pow_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-            if self.verbose:
-                print(f"Terminated perf (pow) process on node \"{HOSTNAME}\".\nOutput: {self.perfpow_process.stdout.read()}")
+        # Create callgraph file
+        if self.perfcall_process:
+            create_callgraph_cmd = ["perf", "script", "-F", "trace:comm,pid,tid,cpu,time,event", "-i", f"{self.save_dir}/{self.temp_perf_file}"]
+            with open(f"{self.save_dir}/{self.call_filename}", "w") as redirect_stdout:
+                subprocess.run(create_callgraph_cmd, stdout=redirect_stdout, stderr=subprocess.STDOUT, text=True)
 
         if self.is_benchmon_control_node:
             print("Dool Control Node: Merging output...")
