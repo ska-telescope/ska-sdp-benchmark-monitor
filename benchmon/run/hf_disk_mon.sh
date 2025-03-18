0697fb09d52a674e515252d0fe7a2f59561a698a
Anass Serhani <anass.serhani@inria.fr>
2025-03-18 19:33:28 +0100

    scoop-418-disk: use awk only to read the disk file

--- benchmon/run/hf_disk_mon.sh
+++ benchmon/run/hf_disk_mon.sh
@@ -4,8 +4,7 @@ freq=$1
 delay=$(bc <<< "scale=6; 1/$freq")
 
 report_disk_stat=$2
-
-# echo "$(cat /proc/diskstats | wc -l)" > $report_disk_stat # or ip link show | grep -c '^[0-9]'
+echo -n "" > $report_disk_stat
 
 echo $(lsblk -d -o NAME --noheadings | wc -l) > $report_disk_stat # Number of major blocks
 echo $(lsblk -o NAME --noheadings | wc -l) >> $report_disk_stat   # Number of all blocks
@@ -13,6 +12,14 @@ echo $(lsblk -d -o NAME,PHY-SEC --noheadings | awk '{printf "%s,%s,", $1, $2}' |
 
 while true
 do
-    awk '{gsub(/ +/, ","); print}' /proc/diskstats | sed 's/^,//' | awk -v timestamp="$(date +'%s.%N')" '{print timestamp","$0}' >> $report_disk_stat
+    awk -v timestamp="$(date +'%s.%N')" '{
+        $1 = $1;
+        printf "%s", timestamp;
+        for (i=1; i<=NF; i++) {
+            printf ",%s", $i;
+        }
+        print "";
+    }' /proc/diskstats >> $report_disk_stat
+
     sleep $delay
 done
