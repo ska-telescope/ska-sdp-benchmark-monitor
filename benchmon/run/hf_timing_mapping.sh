bdd92e773af824999d159ee79cfc53c7a5d4b5dc
Shan Mignot <shan.mignot@oca.eu>
2025-04-02 16:53:52 +0200

    Reformatted journal for readability and easing extraction of data (cmd is not truncated anymore), scripts now sorts journal as a result of SIGTERM

--- benchmon/run/hf_timing_mapping.sh
+++ benchmon/run/hf_timing_mapping.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/bash
 
 freq=$1
-sampl=$(bc <<< "scale=6; 1/$freq")
+output=$2
 
-journal=$2
-echo -n "" > $journal
+# Manage termination
+trap 'echo "#" "$(ps -o ppid,pid,tid,cpuid,etimes,lstart,cmd|head -1)" "$(sort -nk3 -nk5 <(echo "$journal"))" > $output; exit 0' SIGTERM
+
+# Determine sampling period
+sampl=$(bc <<< "scale=6; 1/$freq")
 
 # We assume we want to measure processes running under the same parent process
 root_pid=${PPID}
 # and want to skip the measuring process itself
-meas_pid=$$
+skip_pid=$$
 
 # Get all info from ps to have coherent time stamps
 # Identify what to save to journal with diff (without etime)
 # then save the complete record to have the elapsed time too
 
 # Collect all data with ps to then process it: e all processes, T including threads, o to define output
-# processes have tid matching pid, with -T threads have their parent PID shown.
-# place etime last because all other fields are constant in time (except cpuid because of scheduler) 
+# processes have tid matching pid, with -T threads have their parent PID shown, use ww and place cmd
+# last to have them untruncated
 # ps's native sorting does not seem compatible with listing threads: revert to sort on tid
-psfull="ps --no-header -eTo ppid,pid,tid,cpuid,cmd,lstart,etimes|sort -nk3"
+psfull="ps --no-header -eTwwo ppid,pid,tid,cpuid,etimes,lstart,cmd|sort -nk3"
 
 # Recursively search for child processes and threads with awk
-psold_full="$(eval $psfull | awk -vpp=$root_pid -vp=$meas_pid 'function r(s,e){if(s!=e) {print ps_string[s];s=child_id[s];while(s){sub(",","",s);t=s;sub(",.*","",t);sub("[0-9]+","",s);r(t,e)}}}{child_id[$1]=child_id[$1]","$3;ps_string[$3]=$0}END{r(pp,p)}')"
+psold_full="$(eval $psfull | awk -vpp=$root_pid -vp=$skip_pid 'function r(s,e){if(s!=e) {print ps_string[s];s=child_id[s];while(s){sub(",","",s);t=s;sub(",.*","",t);sub("[0-9]+","",s);r(t,e)}}}{child_id[$1]=child_id[$1]","$3;ps_string[$3]=$0}END{r(pp,p)}')"
 
 # Retain only ppid, pid, tid and cpuid
 psold="$(echo "$psold_full" | awk '{print $1,$2, $3, $4}')"
@@ -33,7 +36,7 @@ psold="$(echo "$psold_full" | awk '{print $1,$2, $3, $4}')"
 while true
 do
     sleep $sampl
-    psnew_full="$(eval $psfull | awk -vpp=$root_pid -vp=$meas_pid 'function r(s,e){if(s!=e) {print ps_string[s];s=child_id[s];while(s){sub(",","",s);t=s;sub(",.*","",t);sub("[0-9]+","",s);r(t,e)}}}{child_id[$1]=child_id[$1]","$3;ps_string[$3]=$0}END{r(pp,p)}')"
+    psnew_full="$(eval $psfull | awk -vpp=$root_pid -vp=$skip_pid 'function r(s,e){if(s!=e) {print ps_string[s];s=child_id[s];while(s){sub(",","",s);t=s;sub(",.*","",t);sub("[0-9]+","",s);r(t,e)}}}{child_id[$1]=child_id[$1]","$3;ps_string[$3]=$0}END{r(pp,p)}')"
     psnew="$(echo "$psnew_full" | awk '{print $1,$2, $3, $4}')"
 
     # Compare psold and psnew with diff
@@ -45,7 +48,10 @@ do
     # be identified based on the fact that this is the last change recorded for this process/thread
     # (sort based on tid: all records execpt for the last correspond to mapping to different cores,
     # and the last to completion).
-    awk 'BEGIN{i=1; j=1} FILENAME==ARGV[1]&&/^[0-9,]+[cd][0-9,]+/ {split($0, a, "[cd]"); n=split(a[1], b, ","); if(n==1) {b[2]=b[1]}; for(k=b[1];k<=b[2];k++) {line[i++]=k}; next} FILENAME==ARGV[2]{psfull[j++]=$0; next} END{for(k=1;k<i;k++) {print psfull[line[k]]}}' <(echo "$difflines") <(echo "$psold_full") >> $journal
+    newlines=$(awk 'BEGIN{i=1; j=1} FILENAME==ARGV[1]&&/^[0-9,]+[cd][0-9,]+/ {split($0, a, "[cd]"); n=split(a[1], b, ","); if(n==1) {b[2]=b[1]}; for(k=b[1];k<=b[2];k++) {line[i++]=k}; next} FILENAME==ARGV[2]{psfull[j++]=$0; next} END{for(k=1;k<i;k++) {print psfull[line[k]]}}' <(echo "$difflines") <(echo "$psold_full"))
+
+    # If something is returned by awk, append to the journal variable with a final new line
+    if [ "$newlines" != "" ]; then journal+="$newlines"; journal+=$'\n'; fi
 
     # Exchange psold and psnew and full entries
     psold=$psnew
